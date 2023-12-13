0291baef559aa25157e1fa0a86e502aa98b23f02
netpyoung <netpyoung@gmail.com>
2023-12-13 10:34:23 +0900

    change: deploy브랜치를 이용하는게 번거로운것 같아, master브랜치를 바로 이용하도록 변경.
    fix: cp coderay-asciidoctor.css public , cp my-asciidoctor.css public

--- to-html.sh
+++ to-html.sh
@@ -2,5 +2,8 @@ mkdir -p public
 asciidoctor -R . -D public -a stylesheet=my-asciidoctor.css clojure-complete-html.adoc **/*.adoc
 mv public/clojure-complete-html.html public/index.html
 cp asciidoctor.css public
+cp coderay-asciidoctor.css public
+cp my-asciidoctor.css public
+
 #asciidoctor clojure-complete-html.adoc **/*.adoc
 #mv clojure-complete-html.html index.html
